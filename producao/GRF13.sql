c1e7efb919cd3c4068858098eea4de404d6cab02
muriloht <muriloht@devcoffee.com.br>
2022-04-12 10:27:41 -0300

    Create GRF13.sql
    COF #12516 - adicionando painel

---
+++ producao/GRF13.sql
@@ -0,0 +1,20 @@
+SELECT
+    prod.name as produto,
+    sum(pl.MovementQty) as qtd_producao
+FROM
+    M_Production p
+LEFT JOIN
+    M_ProductionLine pl ON pl.M_Production_ID = p.M_Production_ID AND pl.IsEndProduct='Y'
+LEFT JOIN
+    M_Product prod ON prod.M_Product_ID = pl.M_Product_ID
+WHERE
+    p.DocStatus IN ('CO','CL')
+AND
+    p.movementdate > now() - INTERVAL '3 months'
+AND
+    p.movementdate < date_trunc('month', CURRENT_DATE)
+GROUP BY
+    produto
+ORDER BY
+    qtd_producao desc
+    
