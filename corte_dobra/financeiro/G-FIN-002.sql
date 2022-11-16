08ed76ef6b64e3997eeff7f1c058ed7a3299d385
brunofereira <113632484+brunofereira@users.noreply.github.com>
2022-11-16 17:36:16 -0300

    Update G-FIN-002.sql

--- corte_dobra/financeiro/G-FIN-002.sql
+++ corte_dobra/financeiro/G-FIN-002.sql
@@ -5,11 +5,10 @@ from
 left join c_bankaccount ba on ba.c_bankaccount_id = p.c_bankaccount_id
     WHERE 
         p.docstatus IN  ('CO', 'CL','RE','VO')
-    AND
-        ba.cof_ComposesCashFlow = 'Y'  
+    AND        ba.cof_ComposesCashFlow = 'Y'  
     AND
         ba.isactive='Y'
 AND
      p.ad_client_id = (SELECT s.ad_client_id
                   from ad_session s 
-                  where s.ad_session_id = {{LOGON}})      
+                  where s.ad_session_id = {{LOGON}})     
