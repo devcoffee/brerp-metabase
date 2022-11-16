2892251caa3aa7d0cd057e6f7bc0c3eafe212f55
brunofereira <bruno.ferreira@devcoffee.com.br>
2022-11-16 17:34:54 -0300

    saldo de geral de caixas e bancos

---
+++ corte_dobra/financeiro/G-FIN-002.sql
@@ -0,0 +1,15 @@
+   select 
+     sum(p.payamt) 
+from 
+     rv_payment p 
+left join c_bankaccount ba on ba.c_bankaccount_id = p.c_bankaccount_id
+    WHERE 
+        p.docstatus IN  ('CO', 'CL','RE','VO')
+    AND
+        ba.cof_ComposesCashFlow = 'Y'  
+    AND
+        ba.isactive='Y'
+AND
+     p.ad_client_id = (SELECT s.ad_client_id
+                  from ad_session s 
+                  where s.ad_session_id = {{LOGON}})      
