36c6d9f37fb850c34aa9f9d98037af5f0c482f0b
brunofereira <bruno.ferreira@devcoffee.com.br>
2022-11-19 14:20:34 -0300

    documentação scripts graficos

--- corte_dobra/financeiro/g-fin-033.sql
+++ corte_dobra/financeiro/g-fin-033.sql
@@ -2,14 +2,14 @@
 ######################################################################################################################################
 GRAFICO: Faturamento dia Corrente - Tipo Operação
 AUTOR: Bruno Luis Ferreira
-COMENTÁRIOS: 
+COMENTÁRIOS:Apura o faturamento de mercadoria do dia, considerando apenas o valor de mercadoria(não inclui impostos não inclusos, fretes, despeesas e seguros),
+ inclui apenas documentos que exibe de em relatórios e qie estejam completados ou fechados.
 O Filtro ocorre apenas por empresa do usuário logado, assim os valore refletem a consolidação de todas as Organizações.
 ######################################################################################################################################
 */
 
 SELECT
     TDD.name as "Tipo Operação",
-    --bp.name as BPname,
     sum(il.cof_linenetamtconverted*i.multiplier) AS valor_total
 FROM 
     rv_c_invoice i
@@ -22,9 +22,7 @@ LEFT JOIN
 where
     il.isdescription = 'N' 
 and
-    i.dateinvoiced =date_trunc('month',current_date)
---and 
---    il.linenetamt > 0
+    trunc(i.dateinvoiced) =trunc(now())
 and 
     i.issotrx = 'Y'
 and 
@@ -33,8 +31,6 @@ and
     il.m_product_id > 0
 and 
     i.cof_ExibirEmRelatorios = 'Y'
---AND 
--- il.m_product_id IS NOT NULL -- Faturas que possuem linha
 AND
    tdd.DocBaseType IN ('ARI' ) -- filtra tipo de documento Normal e devolução
 AND
@@ -43,7 +39,6 @@ AND
                   where s.ad_session_id = {{LOGON}})
 group by 
   "Tipo Operação"
-  --BPname
 order by 
   valor_total desc
     
