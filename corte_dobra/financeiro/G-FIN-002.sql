9f1afaa86c7c853b9fa3dd86952e949b68f9a5b2
brunofereira <bruno.ferreira@devcoffee.com.br>
2022-11-18 22:14:51 -0300

    Documentação e scripts

--- corte_dobra/financeiro/G-FIN-002.sql
+++ corte_dobra/financeiro/G-FIN-002.sql
@@ -1,4 +1,11 @@
-   select 
+/*
+GRAFICO: Saldo geral de caixas e Bancos
+AUTOR: Bruno Luis Ferreira
+COMENTÁRIOS: Soma os pagamentos completados, fechados , estornados e  anulados, de contas que compõem fluo de caixa para criar
+o saldo acumulado das empresas. 
+O Filtro ocorre apenas pela empresa, assim os valore refletem a consolidação de todas as Organizações.
+*/
+select 
      sum(p.payamt) 
 from 
      rv_payment p 
--- corte_dobra/financeiro/g-fin-004.sql
+++ corte_dobra/financeiro/g-fin-004.sql
@@ -1,11 +1,10 @@
 /*
 GRAFICO: Contas a pagar em atraso >= 2 dias
 AUTOR: Bruno Luis Ferreira
-COMENTÁRIOS: Lista os itens em aberto a pagar, convertidos para moeda corrente(R$), vencidos>=2 dias  e que componham fluxo de caixa, 
+COMENTÁRIOS: Lista os itens em aberto a pagar, convertidos para moeda corrente(R$), vencidos> 7 dias e a vencer < 7 dias  e que componham fluxo de caixa, 
 descartando os  memorando de créditos (valor >0).
 O Filtro ocorre apenas pela empresa, assim os valore refletem a consolidação de todas as Organizações.
 */
-
 
 select
   sum(oi.cof_openamtconverted)
@@ -19,7 +18,8 @@ AND
 AND 
     oi.cof_openamtconverted >0
 AND 
-     oi.daysdue > 1 
+     (duedate > now()-7 and duedate < now() + 7 )
+  
 AND
      oi.ad_client_id = (SELECT s.ad_client_id
                   from ad_session s 
