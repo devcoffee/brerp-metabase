46ebbb248faba49715e2bf9a91f0777b7dd9a3c7
brunofereira <bruno.ferreira@devcoffee.com.br>
2022-11-21 22:13:39 -0300

    gfin001  saldo de contas bancarias agrupado

---
+++ clientes/mgs/Financeiro/mgs-gfin-001.sql
@@ -0,0 +1,118 @@
+/*
+######################################################################################################################################
+GRAFICO: Saldos por conta Bancária
+AUTOR: Bruno Luis Ferreira
+COMENTÁRIOS: Soma dos pagamentos completados, fechados , estornados e  anulados, classificando 
+o saldo conciliado, não conciliado e projetado(conciliado + não conciliado)  agrupando por conta corrente e organização. O filtro de org
+aplica na conta bancária por causa de transações de de transferências bancárias.Valores tratatos para  conversão em operações de multimoeda
+######################################################################################################################################
+*/
+
+WITH pagamentos as (
+    SELECT 
+        CASE 
+            WHEN p.isReceipt = 'Y' THEN 
+                 currencyconvert(p.payamt , p.c_currency_id, 297::numeric, p.datetrx::date::timestamp with time zone, p.c_conversiontype_id, p.ad_client_id, p.ad_org_id)
+            ELSE 
+                 currencyconvert((  p.payamt * -1 ), p.c_currency_id, 297::numeric, p.datetrx::date::timestamp with time zone, p.c_conversiontype_id, p.ad_client_id, p.ad_org_id)
+        END as Valor,
+        CASE 
+            WHEN p.isreconciled = 'N'  THEN
+                'NCC'
+            ELSE
+                'CC'
+        END as Tipo,
+        ba.name as Conta,
+        org.name as orgname,
+        bb.name as BancoName
+    FROM 
+        C_Payment p 
+    LEFT JOIN
+        C_BankAccount ba ON ba.C_BankAccount_ID = p.C_BankAccount_ID
+    LEFT JOIN
+       C_Bank bb ON bb.C_Bank_ID=ba.C_Bank_ID
+        
+    LEFT JOIN 
+        ad_org org  on org.ad_org_id=ba.ad_org_id
+    WHERE 
+        p.docstatus IN  ('CO', 'CL','RE','VO')
+    AND
+        ba.cof_ComposesCashFlow = 'Y' 
+    AND
+        ba.isactive='Y'
+AND
+     p.ad_client_id = (SELECT s.ad_client_id
+                  from ad_session s 
+                  where s.ad_session_id = {{LOGON}})        
+)
+SELECT 
+    p.BancoName as banco,
+    p.orgname,
+    p.conta,
+    sum(case when p.Tipo='CC' then p.valor else 0 end) as  "Valor Conciliado",
+    sum(case when p.tipo='NCC' then p.valor  else 0 end) as "Valor Não Conciliado",
+    sum(p.valor) as "Saldo Projetado" 
+    
+FROM 
+    pagamentos p
+GROUP by
+       p.orgname, banco,p.conta
+ORDER BY
+    p.orgname, banco,p.conta,"Saldo Projetado" asc
+WITH pagamentos as (
+    SELECT 
+        CASE 
+            WHEN p.isReceipt = 'Y' THEN 
+                 currencyconvert(p.payamt , p.c_currency_id, 297::numeric, p.datetrx::date::timestamp with time zone, p.c_conversiontype_id, p.ad_client_id, p.ad_org_id)
+            ELSE 
+                 currencyconvert((  p.payamt * -1 ), p.c_currency_id, 297::numeric, p.datetrx::date::timestamp with time zone, p.c_conversiontype_id, p.ad_client_id, p.ad_org_id)
+        END as Valor,
+        CASE 
+            WHEN p.isreconciled = 'N'  THEN
+                'NCC'
+            ELSE
+                'CC'
+        END as Tipo,
+        ba.name as Conta,
+        org.name as orgname,
+        bb.name as BancoName
+    FROM 
+        C_Payment p 
+    LEFT JOIN
+        C_BankAccount ba ON ba.C_BankAccount_ID = p.C_BankAccount_ID
+    LEFT JOIN
+       C_Bank bb ON bb.C_Bank_ID=ba.C_Bank_ID
+        
+  LEFT JOIN 
+        ad_org org  on org.ad_org_id=ba.ad_org_id
+    WHERE 
+        p.docstatus IN  ('CO', 'CL')
+   -- AND
+      --  ba.cof_ComposesCashFlow = 'Y' 
+    AND
+        ba.isactive='Y'
+AND
+     (case WHEN {{TipoP}}='01' then 
+                        ba.ad_org_id IN (1000001) 
+           WHEN {{TipoP}}='02' then 
+                        ba.ad_org_id IN (5000000) 
+           WHEN {{TipoP}}='03' then 
+                        ba.ad_org_id IN (5000004) 
+           else 
+                        ba.ad_org_id IN (5000004,5000000,1000001)
+           end )     
+)
+SELECT 
+    p.BancoName as banco,
+    p.orgname,
+    p.conta,
+    sum(case when p.Tipo='CC' then p.valor else 0 end) as  "Valor Conciliado",
+    sum(case when p.tipo='NCC' then p.valor  else 0 end) as "Valor Não Conciliado",
+    sum(p.valor) as "Saldo Projetado" 
+    
+FROM 
+    pagamentos p
+GROUP by
+       p.orgname, banco,p.conta
+ORDER BY
+  "Saldo Projetado" desc
